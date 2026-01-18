fd43f990149a42e00b9de0f2e225267a962df288
Manya <e1459283@u.nus.edu>
2026-01-18 10:23:52 +0800

    Implemented the initial skeleton version

--- src/main/java/AgentSmith.java
+++ src/main/java/AgentSmith.java
@@ -1,4 +1,7 @@
 public class AgentSmith {
+
+    public static String name = "Agent Smith";
+
     public static void main(String[] args) {
         String logo = "        _                    _     ____            _ _   _       \n" +
                 "       / \\   __ _  ___ _ __ | |_  / ___| _ __ ___ (_) |_| |__   \n" +
@@ -6,6 +9,22 @@ public class AgentSmith {
                 "     / ___ \\ (_| |  __/ | | | |_   ___) | | | | | | | |_| | | |  \n" +
                 "    /_/   \\_\\__, |\\___|_| |_|\\__| |____/|_| |_| |_|_|\\__|_| |_|  \n" +
                 "             |___/                                                \n";
+
         System.out.println("Hello from\n" + logo);
+
+        print_intro();
+        print_bye();
+    }
+
+    public static void print_intro() {
+        System.out.println("________________________________________________________");
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________________________");
+    }
+
+    public static void print_bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________________________");
     }
 }
